10996aff45270cc6f205087fe5cf32716e530280
kevnalfyolo <kevinalifiano14206@gmail.com>
2025-09-04 16:09:14 +0700

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,10 @@
+import random
+
+print("Your password is: ")
+
+chars = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*"
+
+password = ''
+for x in range(16):
+    password += random.choice(chars)
+print(password)
