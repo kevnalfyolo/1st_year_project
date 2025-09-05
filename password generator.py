bc950a7eb4874cc4d9ed6a786a3f2270600c99fc
kevnalfyolo <kevinalifiano14206@gmail.com>
2025-09-05 21:24:25 +0700

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -1,22 +1,24 @@
 import random
 
-print("Your password is: ")
+print("Welcome to Password Generator🤗")
+digit = int(input("How long password that you need?(In digits): "))
+(print("Here you go!, your password is: "))
 char = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*"
 
 password = ''
-for x in range(16):
+for x in range(digit):
   password += random.choice(char)
 print(password)
 repetition = ''
 while True:
   repetition = input("Try again? (yes/no): ")
   if repetition == "no":
-    print("Thank you for using our program!")
-    break
+      print("Thank you for using our program!🙌")
+      break
   elif repetition == "yes":
-    password = ''
-    for x in range(16):
-      password += random.choice(char)
-    print(password)
+      password = ''
+      for x in range(digit):
+          password += random.choice(char)
+      print(password)
   else:
-    print("Error!")
+      print("Error!")
